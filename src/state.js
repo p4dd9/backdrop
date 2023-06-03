c0fdd0ff0dc7e58aaa7ec6de1ce6edf5e10b1fad
Patrick Obermueller <pat.obermueller@gmail.com>
2023-06-03 12:38:10 +0200

    wip-feature: update input vales on state change

--- src/state.js
+++ src/state.js
@@ -16,4 +16,24 @@ const resetBackdropState = () => {
 const updateBackdropFilterCSS = () => {
     const backdropFilterCSS = Object.values(backdropState).join(' ');
     overlayFilterDOM.style.backdropFilter = backdropFilterCSS;
+    updateCssFilterInputValues();
+};
+
+const updateCssFilterInputValues = () => {
+    for (const key of Object.keys(backdropState)) {
+        const domInputElement = document.getElementById(key);
+        if (domInputElement) {
+            const pureValue = backdropState[key]
+                .split('(')
+                .pop()
+                .split(')')[0]
+                .replace(/[^0-9.,]/g, '');
+            /**
+             * TODO: fix this for number decimals
+             */
+            console.log(pureValue);
+            domInputElement.value = parseFloat(pureValue);
+        }
+    }
+    console.log(backdropState);
 };
